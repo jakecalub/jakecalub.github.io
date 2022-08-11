3b5c71613ad044b138850035406754cb1987b43c
jakecalub <77728200+jakecalub@users.noreply.github.com>
2022-08-11 15:55:04 +1000

    update

---
+++ script/script.js
@@ -0,0 +1,63 @@
+function validateLogIn(){
+    var name = document.forms["contact_form"]["name"].value
+    var email = document.forms["contact_form"]["email"].value
+    var message = document.forms["contact_form"]["message"].value
+    if(name == null || name == ""){
+        alert("Name cannot be empty!");
+        return false;
+    }else if(email == null || email==""){
+        alert("Email cannot be empty!");
+        return false;
+    }else if(message == null || message == ""){
+        alert("Message cannot be empty!");
+        return false;
+    }else{
+        alert("Message successfully sent!");
+        return true;
+    }
+}
+
+function displayModal(imgId){
+    var modal = document.getElementById("myModal");
+
+    var img = document.getElementById(imgId);
+    var modalImg = document.getElementById("imgBlock");
+    var captionText = document.getElementById("caption");
+    img.onclick = function(){
+        modal.style.display = "block";
+        modalImg.src = this.src;
+        if(this.alt == undefined){
+            captionText.innerHTML = this.title;
+        }else{
+            captionText.innerHTML = this.alt;
+        }   
+    }
+
+    // Get the <span> element that closes the modal
+    var span = document.getElementsByClassName("close")[0];
+
+    // When the user clicks on <span> (x), close the modal
+    span.onclick = function() { 
+    modal.style.display = "none";
+    }
+}
+
+function onFocusAnimation(hobby){
+    if(hobby == "1"){
+        document.getElementById("baking").src="img/hobbies/baking_hover.png";
+    }else if(hobby == "2"){
+        document.getElementById("lettering").src="img/hobbies/calligraphy_hover.png";
+    }else{
+        document.getElementById("camera").src="img/hobbies/camera_hover.png";
+    }
+}
+
+function unfocusAnimation(hobby){
+    if(hobby == "1"){
+        document.getElementById("baking").src="img/hobbies/baking.png";
+    }else if(hobby == "2"){
+        document.getElementById("lettering").src="img/hobbies/calligraphy.png";
+    }else{
+        document.getElementById("camera").src="img/hobbies/camera.png";
+    }
+}
